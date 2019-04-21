c3628bcb22796f0f9898080f809bd726d37143a9
Terem <staskov2009@mail.ru>
2019-04-21 14:54:33 +0300

    Поправил косяки #2

--- Lesson 1/weight.rb
+++ Lesson 1/weight.rb
@@ -9,6 +9,5 @@ perfect_weight = height - 110
 if perfect_weight <= 0
   puts "#{name}, Ваш идеальный вес, #{perfect_weight}." #до этого я задание криво прочел, ищем же идеальный вес, а не лишний =)
 else
-  excess_weight = height - 110
   puts "Ваш вес уже оптимальный"
 end
